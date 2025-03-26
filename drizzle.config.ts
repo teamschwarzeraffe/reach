4c7d5b97e3d119ce384b413691199b07ab13b2bf
teamschwarzeraffe <monkeyandblack@yahoo.com>
2025-03-26 23:38:32 +0000

    Fix styles - fixing typo

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 // drizzle.config.ts
 import type { Config } from "drizzle-kit";
 
-const { LOCAL_DB_PATH, DB_ID, D1_TOKEN, CF_ACCOUNT_ID } = process.env;
+let { LOCAL_DB_PATH, DB_ID, D1_TOKEN, CF_ACCOUNT_ID } = process.env;
 
 console.log("LOCAL_DB_PATH", LOCAL_DB_PATH);
 
